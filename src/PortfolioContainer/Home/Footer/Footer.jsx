41e91b7f72d9535c8f7f1cdcedaf195a82c6db14
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2022-12-26 23:26:14 -0600

    Add files via upload

---
+++ src/PortfolioContainer/Home/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <div className="footer-parent">
+        <img
+          src={require("../../../assets/Home/shape-bg.png")}
+          alt="sin conexión a internet"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
