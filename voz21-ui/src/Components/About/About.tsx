0ce0b8dc720ecf84c44f7637641f450394ac97ff
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2023-08-22 16:36:02 -0600

    about responsive

--- voz21-ui/src/Components/About/About.tsx
+++ voz21-ui/src/Components/About/About.tsx
@@ -6,16 +6,23 @@ import "./about.css";
 export const About: React.FC = () => {
   return (
     <div className="h-screen about bg-gray-300 ">
-      <h1 className="text-5xl font-bold my-20">¿Quienes Somos?</h1>
+      <h1 className="text-5xl title font-bold my-20 mx-auto">
+        ¿Quienes Somos?
+      </h1>
       <div className="flex container items-center flex-wrap">
-        <h3 className="max-w-lg mx-auto mb-5">
+        <h3 className="max-w-lg mx-auto mb-5 text-justify">
           Explicación de quienes somos Lorem, ipsum dolor sit amet consectetur
           adipisicing elit. Repudiandae sit magnam ducimus iste quia quaerat?
           Excepturi omnis dolor amet sit hic sint magni veritatis quia
           aspernatur itaque! Nisi, non architecto.
         </h3>
         <div className="max-w-lg mx-auto mt-5">
-          <Image src={Us} width={400} alt="Equipo de locutores" />
+          <Image
+            src={Us}
+            width={400}
+            alt="Equipo de locutores"
+            objectFit="true"
+          />
         </div>
       </div>
     </div>
