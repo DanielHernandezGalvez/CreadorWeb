3fedeb78671715cdb0c313984cdefe13461cb4f7
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2023-08-22 16:07:44 -0600

    header y navbar

--- voz21-ui/src/Components/Header/Header.tsx
+++ voz21-ui/src/Components/Header/Header.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 import "./header.css";
+import Link from "next/link";
 
-export const Header = () => {
+export const Header: React.FC = () => {
   return (
     <div className="main-header ">
       <h1 className="main-header-title font-bold text-center text-white">
         Voz <span className="text-yellow-500">21</span>
       </h1>
+      <h2 className="text-white  text-4xl mb-10">Tu voz</h2>
+      <Link
+        href="./"
+        className="inline-block px-6 py-3 bg-yellow-500 hover:bg-yellow-700 text-white font-semibold rounded-xl transition duration-300"
+      >
+        Contáctanos
+      </Link>
     </div>
   );
 };
