db2b9c917a28c943beb641f67f7955d99e9f8fbd
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2023-08-23 16:44:48 -0600

    filter

--- voz21-ui/src/Components/Catalogue/Accordion.tsx
+++ voz21-ui/src/Components/Catalogue/Accordion.tsx
@@ -3,7 +3,7 @@
 import AccordionItem from "./AccordionItem";
 const Accordion: React.FC = () => {
   return (
-    <div className="h-screen about bg-white">
+    <div className=" bg-white">
       <div className="container mx-auto p-4">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 ">
           <div>
--- voz21-ui/src/Components/Catalogue/Catalogue.tsx
+++ voz21-ui/src/Components/Catalogue/Catalogue.tsx
@@ -4,7 +4,15 @@ import Accordion from "./Accordion";
 
 const Catalogue: React.FC = () => {
   return (
-    <div>
+    <div className="flex justify-center items-center h-screen flex-col">
+      <h2 className="text-5xl font-bold text-center my-10">
+        Catálogo de voces
+      </h2>
+      <select className="select pe-5 border rounded">
+        <option value="Mujer">Mujer</option>
+        <option value="Hombre">Hombre</option>
+        <option value="Inglés">Inglés</option>
+      </select>
       <Accordion />
     </div>
   );
