c0e53633c6ccf655c85298e8608abb5eb0ff1f26
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2023-09-25 13:20:15 -0600

    gallery

--- voz21-ui/src/Components/Gallery/Gallery.tsx
+++ voz21-ui/src/Components/Gallery/Gallery.tsx
@@ -3,15 +3,16 @@ import React from "react";
 const Gallery = () => {
   return (
     <div>
-      {" "}
+ 
       <div className="container mx-auto px-5 py-2 lg:px-32 lg:pt-24">
         <div className="-m-1 flex flex-wrap md:-m-2">
           <div className="flex w-1/2 flex-wrap">
             <div className="w-1/2 p-1 md:p-2">
               <img
                 alt="gallery"
-                className="block h-full w-full rounded-lg object-cover object-center"
+                className="block h-full w-full rounded-lg object-cover object-center img-with-title"
                 src="/gallery/0705.jpeg"
+                title="Daniel Hernández"
               />
             </div>
             <div className="w-1/2 p-1 md:p-2">
@@ -53,6 +54,56 @@ const Gallery = () => {
             </div>
           </div>
         </div>
+
+        <div className="-m-1 flex flex-wrap md:-m-2">
+          <div className="flex w-1/2 flex-wrap">
+            <div className="w-1/2 p-1 md:p-2">
+              <img
+                alt="gallery"
+                className="block h-full w-full rounded-lg object-cover object-center"
+                src="/gallery/2003.jpeg"
+              />
+            </div>
+            <div className="w-1/2 p-1 md:p-2">
+              <img
+                alt="gallery"
+                className="block h-full w-full rounded-lg object-cover object-center"
+                src="/gallery/1902.jpeg"
+              />
+            </div>
+            <div className="w-full p-1 md:p-2">
+              <img
+                alt="gallery"
+                className="block h-full w-full rounded-lg object-cover object-center"
+                src="/gallery/1801.jpeg"
+              />
+            </div>
+          </div>
+          <div className="flex w-1/2 flex-wrap">
+            <div className="w-full p-1 md:p-2">
+              <img
+                alt="gallery"
+                className="block h-full w-full rounded-lg object-cover object-center"
+                src="/gallery/1703.jpeg"
+              />
+            </div>
+            <div className="w-1/2 p-1 md:p-2">
+              <img
+                alt="gallery"
+                className="block h-full w-full rounded-lg object-cover object-center"
+                src="/gallery/1602.jpeg"
+              />
+            </div>
+            <div className="w-1/2 p-1 md:p-2">
+              <img
+                alt="gallery"
+                className="block h-full w-full rounded-lg object-cover object-center"
+                src="/gallery/1502.jpeg"
+              />
+            </div>
+          </div>
+        </div>
+
       </div>
     </div>
   );
