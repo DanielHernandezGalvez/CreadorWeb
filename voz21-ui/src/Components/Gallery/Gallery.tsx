3a927451fd4e4e3669730af8300d62d2a0808e07
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2023-10-11 11:37:54 -0600

    locutores agregados

--- voz21-ui/src/Components/Gallery/Gallery.tsx
+++ voz21-ui/src/Components/Gallery/Gallery.tsx
@@ -1,29 +1,22 @@
 "use client";
 import "./gallery.css";
 import Image from "next/image"; // Importa el componente Image de Next.js
+import {GalleryProps} from "@/interfaces/GalleryProps"
+import locutoresData from "../../data/locutoresData.json"
 
-const Gallery: React.FC = () => {
-  const images = [
-    { src: "/gallery/0501.jpeg", alt: "Image 1" },
-    { src: "/gallery/0601.jpeg", alt: "Image 2" },
-    { src: "/gallery/0705.jpeg", alt: "Image 3" },
-    { src: "/gallery/0805.jpeg", alt: "Image 3" },
-    { src: "/gallery/1404.jpeg", alt: "Image 3" },
-    { src: "/gallery/1502.jpeg", alt: "Image 3" },
-    { src: "/gallery/1601.jpeg", alt: "Image 3" },
-    { src: "/gallery/1703.jpeg", alt: "Image 3" },
-    { src: "/gallery/1801.jpeg", alt: "Image 3" },
-    // Agrega más imágenes si es necesario
-  ];
+const Gallery: React.FC<GalleryProps> = ({
+  img
+}) => {
+
 
   return (
     <>
       <div className="container flex justify-center py-5 gap-10 flex-wrap">
-        {images.map((image, index) => (
+        {locutoresData.map((image, index) => (
           <Image
             key={index}
-            src={image.src}
-            alt={image.alt}
+            src={image.img}
+            alt="img"
             width={250}
             height={250}
             className="rounded-lg transition-transform transform scale-100 hover:scale-110"
