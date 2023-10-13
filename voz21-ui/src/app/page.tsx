6819fe67cabc4f2d0d9184cda12eaf4cf3246b19
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2023-10-13 11:13:30 -0600

    firt demo

--- voz21-ui/src/app/page.tsx
+++ voz21-ui/src/app/page.tsx
@@ -1,4 +1,5 @@
-import Image from "next/image";
+"use client"
+import React, { useState } from "react";
 import Head from "next/head";
 import Navigation from "../Components/Navigation/Navigation";
 import { Header } from "@/Components/Header/Header";
@@ -10,7 +11,37 @@ import Footer from "@/Components/Footer/Footer";
 import WhatsAppIcon from "@/Components/WhatsAppIcon/WhatsAppIcon";
 import Gallery from "@/Components/Gallery/Gallery";
 
+type Section = "inicio" | "servicios" | "catalogo" | "locutores" | "directora";
+
 export default function Home() {
+  const [inicio, setInicio] = useState(true);
+  const [servicios, setServicios] = useState(false);
+  const [catalogo, setCatalogo] = useState(false);
+  const [locutores, setLocutores] = useState(false);
+  const [directora, setDirectora] = useState(false);
+
+  const handleSectionChange = (section: Section) => {
+    // Resetea todos los estados a false
+    setInicio(false);
+    setServicios(false);
+    setCatalogo(false);
+    setLocutores(false);
+    setDirectora(false);
+
+    // Establece el estado correspondiente en función de la sección
+    if (section === "inicio") {
+      setInicio(true);
+    } else if (section === "servicios") {
+      setServicios(true);
+    } else if (section === "catalogo") {
+      setCatalogo(true);
+    } else if (section === "locutores") {
+      setLocutores(true);
+    } else if (section === "directora") {
+      setDirectora(true);
+    }
+  };
+
   return (
     <>
       <div className="bg-white min-h-screen">
@@ -18,15 +49,18 @@ export default function Home() {
           <title>Voz 21</title>
           <meta
             name="description"
-            content="Agencia de voces para locucion y comerciales"
+            content="Agencia de voces para locución y comerciales"
           />
         </Head>
-        <Navigation />
+        <Navigation  />
         <WhatsAppIcon />
         <Header subtitle="Tu Voz" linkText="Ver Catálogo" linkUrl="#catalogue" />
         <About />
-        {/* <Gallery /> */}
         <Catalogue />
+         <Customers />
+         <Contact />
+        <Gallery />
+         <Footer />
         <Customers />
         <Contact />
         <Footer />
