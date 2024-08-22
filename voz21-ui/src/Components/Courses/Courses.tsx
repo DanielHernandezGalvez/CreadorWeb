1b91bef8571a1ca8fb8af51f8f777f5ec2e0619e
unknown <hernandezgalvezalejandro@gmail.com>
2024-08-22 16:15:02 -0600

    courses

--- voz21-ui/src/Components/Courses/Courses.tsx
+++ voz21-ui/src/Components/Courses/Courses.tsx
@@ -14,7 +14,6 @@ const Courses: React.FC = () => {
             title="Producción y comercialización de rádio"
             description="Costo 3000 pesos"
             imageUrl="/courses/taller-radio.jpg"
-            // date="04/05/2024 de 09:00 a 11:00 Hrs."
             video=""
           />
           <CourseCard
