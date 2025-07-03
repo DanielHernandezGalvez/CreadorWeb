6a9e4810409ef45abd5f2ffd9a853f7dd635e2d1
unknown <hernandezgalvezalejandro@gmail.com>
2025-07-02 22:16:37 -0600

    cambio en el mapa para tener mayor seguridad

--- voz21-ui/src/Components/Contact/Contact.tsx
+++ voz21-ui/src/Components/Contact/Contact.tsx
@@ -72,7 +72,7 @@ const Contact: React.FC = () => {
       </div>
       <div className="w-full h-96 ">
         <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3732.7752267518463!2d-103.3685103!3d20.6787217!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8428ae1a5f491ce1%3A0x42105a2009e2270!2sAv.%20Chapultepec%20Nte.%20140-interior%20202%2C%20Ladr%C3%B3n%20de%20Guevara%2C%20Americana%2C%2044600%20Guadalajara%2C%20Jal.!5e0!3m2!1ses!2smx!4v1692895531736!5m2!1ses!2smx"
+          src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d1109.766622938275!2d-103.36998104396514!3d20.677913507559893!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses!2smx!4v1751515862861!5m2!1ses!2smx"
           width="100%"
           height="450"
           allowFullScreen
